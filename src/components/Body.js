21470e2d5b63ee24aab16a254dc24fddeed842f0
saijamii <saijami38@gmail.com>
2024-06-11 09:31:36 +0530

    router for WatchPage

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,12 +1,12 @@
 import React from "react";
 import Siderbar from "./Siderbar";
-import MainContainer from "./MainContainer";
+import { Outlet } from "react-router-dom";
 
 const Body = () => {
   return (
     <div className="flex">
       <Siderbar />
-      <MainContainer />
+      <Outlet />
     </div>
   );
 };
