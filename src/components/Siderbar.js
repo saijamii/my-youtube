e3e626fdde1b2f52c381df5ff8aa4687af587226
saijamii <saijami38@gmail.com>
2024-06-12 09:29:12 +0530

    side changes

--- src/components/Siderbar.js
+++ src/components/Siderbar.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { useSelector } from "react-redux";
+import { Link } from "react-router-dom";
 
 const Siderbar = () => {
   const isMenuOPen = useSelector((store) => store.app.isMenuOpen);
@@ -7,7 +8,9 @@ const Siderbar = () => {
   return (
     <div className="p-5  shadow-lg col-span-1 w-48">
       <ul>
-        <li>Home</li>
+        <li>
+          <Link to={"/"}>Home</Link>
+        </li>
         <li>Shorts</li>
         <li>Videos</li>
         <li>Live</li>
