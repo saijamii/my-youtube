85f46bb3ac9a7672a1d0eb7c67daa33236ee9d14
saijami <saijami@unosimple.com>
2024-06-12 19:03:52 +0530

    dynamic width

--- src/components/VideoCard.js
+++ src/components/VideoCard.js
@@ -1,14 +1,14 @@
 import React from "react";
-// import { useSelector } from "react-redux";
+import { useSelector } from "react-redux";
 
 const VideoCard = ({ info }) => {
   const { snippet, statistics } = info;
   const { channelTitle, title, thumbnails } = snippet;
-  // const isMenuOpen = useSelector((store) => store.app.isMenuOpen);
-  // const cardWidth = isMenuOpen ? "w-64" : "w-80";
+  const isMenuOpen = useSelector((store) => store.app.isMenuOpen);
+  const cardWidth = isMenuOpen ? "w-64" : "w-70";
 
   return (
-    <div className="p-2 m-2 w-72 shadow-lg">
+    <div className={`p-2 m-2 ${cardWidth} max-w-65 shadow-lg`}>
       <img className="rounded-lg" alt="thumbnail" src={thumbnails.medium.url} />
       <ul>
         <li className="font-bold py-2">{title}</li>
