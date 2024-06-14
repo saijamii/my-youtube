21914e9d8f84885eba910505f8ab08fba5300b0b
saijamii <saijami38@gmail.com>
2024-06-14 08:52:12 +0530

    changes

--- src/components/Buttons.js
+++ src/components/Buttons.js
@@ -1,7 +1,6 @@
 import React from "react";
 
 const Buttons = ({ name }) => {
-  console.log(name, "name");
   return (
     <div>
       <button className="p-2 m-2 px-5 py-2 w-max rounded-lg bg-gray-200">
