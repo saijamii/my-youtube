ecd95a8f204b06afe67b3fa780f18a3b7ef67d36
saijamii <saijami38@gmail.com>
2024-06-16 14:43:57 +0530

    ChatMessage

--- src/components/LiveChat.js
+++ src/components/LiveChat.js
@@ -1,9 +1,40 @@
-import React from "react";
+import React, { useEffect } from "react";
+import ChatMessage from "./ChatMessage";
 
 function LiveChat() {
+  useEffect(() => {
+    const i = setInterval(() => {
+      console.log("III")
+    }, 2000);
+
+    return () => clearInterval(i);
+  }, []);
+
   return (
-    <div className="w-full h-[525px] ml-2 p-2 border border-black ">
-      LiveChat
+    <div className="w-full h-[525px] ml-2 p-2 border border-black bg-slate-100 rounded-lg overflow-y-scroll ">
+      <ChatMessage name={"SJ"} message={"This is Live Comment"} />
+      <ChatMessage name={"SJ"} message={"This is Live Comment"} />
+      <ChatMessage name={"SJ"} message={"This is Live Comment"} />
+      <ChatMessage name={"SJ"} message={"This is Live Comment"} />
+      <ChatMessage name={"SJ"} message={"This is Live Comment"} />
+      <ChatMessage name={"SJ"} message={"This is Live Comment"} />
+      <ChatMessage name={"SJ"} message={"This is Live Comment"} />
+      <ChatMessage name={"SJ"} message={"This is Live Comment"} />
+      <ChatMessage name={"SJ"} message={"This is Live Comment"} />
+      <ChatMessage name={"SJ"} message={"This is Live Comment"} />
+      <ChatMessage name={"SJ"} message={"This is Live Comment"} />
+      <ChatMessage name={"SJ"} message={"This is Live Comment"} />
+      <ChatMessage name={"SJ"} message={"This is Live Comment"} />
+      <ChatMessage name={"SJ"} message={"This is Live Comment"} />
+      <ChatMessage name={"SJ"} message={"This is Live Comment"} />
+      <ChatMessage name={"SJ"} message={"This is Live Comment"} />
+      <ChatMessage name={"SJ"} message={"This is Live Comment"} />
+      <ChatMessage name={"SJ"} message={"This is Live Comment"} />
+      <ChatMessage name={"SJ"} message={"This is Live Comment"} />
+      <ChatMessage name={"SJ"} message={"This is Live Comment"} />
+      <ChatMessage name={"SJ"} message={"This is Live Comment"} />
+      <ChatMessage name={"SJ"} message={"This is Live Comment"} />
+      <ChatMessage name={"SJ"} message={"This is Live Comment"} />
     </div>
   );
 }
