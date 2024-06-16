25a00ddc6cc6544140dc5100ea47ac4c987c8444
saijamii <saijami38@gmail.com>
2024-06-16 15:09:17 +0530

    LiveChat init

--- src/components/ChatMessage.js
+++ src/components/ChatMessage.js
@@ -1,15 +1,14 @@
 const ChatMessage = ({ name, message }) => {
-    return (
-      <div className="flex items-center shadow-sm p-2">
-        <img
-          className="h-7"
-          alt="user"
-          src="https://www.iconpacks.net/icons/2/free-user-icon-3296-thumb.png"
-        />
-        <span className="font-bold px-2">{name}</span>
-        <span>{message}</span>
-      </div>
-    );
-  };
-  export default ChatMessage;
-  
+  return (
+    <div className="flex items-center shadow-sm p-2">
+      <img
+        className="h-7"
+        alt="user"
+        src="https://www.iconpacks.net/icons/2/free-user-icon-3296-thumb.png"
+      />
+      <span className="font-bold px-0.5">{name}</span>
+      <span className="text-sm">{message}</span>
+    </div>
+  );
+};
+export default ChatMessage;
--- src/components/LiveChat.js
+++ src/components/LiveChat.js
@@ -1,40 +1,34 @@
 import React, { useEffect } from "react";
 import ChatMessage from "./ChatMessage";
+import { useDispatch, useSelector } from "react-redux";
+import { addMessage } from "../Utils/chatSlice";
 
 function LiveChat() {
+  const dispatch = useDispatch();
+  const chatMessages = useSelector((store) => store.chat.messages);
+  console.log(chatMessages, "chatMessages");
   useEffect(() => {
+    let id = 1;
     const i = setInterval(() => {
-      console.log("III")
+      dispatch(
+        addMessage({
+          name: "SJ",
+          message: "Hey this is Live Comment " + id++,
+        })
+      );
     }, 2000);
 
     return () => clearInterval(i);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   return (
-    <div className="w-full h-[525px] ml-2 p-2 border border-black bg-slate-100 rounded-lg overflow-y-scroll ">
-      <ChatMessage name={"SJ"} message={"This is Live Comment"} />
-      <ChatMessage name={"SJ"} message={"This is Live Comment"} />
-      <ChatMessage name={"SJ"} message={"This is Live Comment"} />
-      <ChatMessage name={"SJ"} message={"This is Live Comment"} />
-      <ChatMessage name={"SJ"} message={"This is Live Comment"} />
-      <ChatMessage name={"SJ"} message={"This is Live Comment"} />
-      <ChatMessage name={"SJ"} message={"This is Live Comment"} />
-      <ChatMessage name={"SJ"} message={"This is Live Comment"} />
-      <ChatMessage name={"SJ"} message={"This is Live Comment"} />
-      <ChatMessage name={"SJ"} message={"This is Live Comment"} />
-      <ChatMessage name={"SJ"} message={"This is Live Comment"} />
-      <ChatMessage name={"SJ"} message={"This is Live Comment"} />
-      <ChatMessage name={"SJ"} message={"This is Live Comment"} />
-      <ChatMessage name={"SJ"} message={"This is Live Comment"} />
-      <ChatMessage name={"SJ"} message={"This is Live Comment"} />
-      <ChatMessage name={"SJ"} message={"This is Live Comment"} />
-      <ChatMessage name={"SJ"} message={"This is Live Comment"} />
-      <ChatMessage name={"SJ"} message={"This is Live Comment"} />
-      <ChatMessage name={"SJ"} message={"This is Live Comment"} />
-      <ChatMessage name={"SJ"} message={"This is Live Comment"} />
-      <ChatMessage name={"SJ"} message={"This is Live Comment"} />
-      <ChatMessage name={"SJ"} message={"This is Live Comment"} />
-      <ChatMessage name={"SJ"} message={"This is Live Comment"} />
+    <div className="w-full h-[525px] ml-2 p-2 border border-black bg-slate-100 rounded-lg overflow-y-scroll flex flex-col-reverse">
+      <div>
+        {chatMessages.map((c, i) => (
+          <ChatMessage key={i} name={c.name} message={c.message} />
+        ))}
+      </div>
     </div>
   );
 }
