934fc2fa55d9d11aeed92ca735e8daf1e0e3040d
saijamii <saijami38@gmail.com>
2024-06-16 22:24:52 +0530

    Send Messaage

--- src/components/LiveChat.js
+++ src/components/LiveChat.js
@@ -1,13 +1,13 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import ChatMessage from "./ChatMessage";
 import { useDispatch, useSelector } from "react-redux";
 import { addMessage } from "../Utils/chatSlice";
 import { generateRandomMessage, generateRandomName } from "../Utils/helper";
 
 function LiveChat() {
+  const [liveMessage, setLiveMessage] = useState("");
   const dispatch = useDispatch();
   const chatMessages = useSelector((store) => store.chat.messages);
-  console.log(chatMessages, "chatMessages");
   useEffect(() => {
     let id = 1;
     const i = setInterval(() => {
@@ -23,14 +23,41 @@ function LiveChat() {
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
+  const handleLiveSubmit = (e) => {
+    e.preventDefault();
+    dispatch(
+      addMessage({
+        name: "SJ",
+        message: liveMessage,
+      })
+    );
+    setLiveMessage("");
+  };
+
   return (
-    <div className="w-full h-[525px] ml-2 p-2 border border-black bg-slate-100 rounded-lg overflow-y-scroll flex flex-col-reverse">
-      <div>
-        {chatMessages.map((c, i) => (
-          <ChatMessage key={i} name={c.name} message={c.message} />
-        ))}
+    <>
+      <div className="w-full h-[525px] ml-2 p-2 border border-black bg-slate-100 rounded-lg overflow-y-scroll flex flex-col-reverse">
+        <div>
+          {chatMessages.map((c, i) => (
+            <ChatMessage key={i} name={c.name} message={c.message} />
+          ))}
+        </div>
       </div>
-    </div>
+      <form
+        className=" flex w-full p-2 ml-2 border border-black"
+        onSubmit={handleLiveSubmit}
+      >
+        <input
+          className="w-full px-2"
+          type="text"
+          onChange={(e) => {
+            setLiveMessage(e.target.value);
+          }}
+          value={liveMessage}
+        />
+        <button className="px-2 mx-4 bg-green-300">Send</button>
+      </form>
+    </>
   );
 }
 
