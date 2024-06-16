53498a5625b54fd94b9b0398ddaf12a0d8a7dcd4
saijamii <saijami38@gmail.com>
2024-06-16 14:43:44 +0530

    LiveChat

---
+++ src/components/LiveChat.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function LiveChat() {
+  return (
+    <div className="w-full h-[525px] ml-2 p-2 border border-black ">
+      LiveChat
+    </div>
+  );
+}
+
+export default LiveChat;
--- src/components/WatchPage.js
+++ src/components/WatchPage.js
@@ -3,6 +3,7 @@ import { useDispatch } from "react-redux";
 import { closeMenu } from "../Utils/appSlice";
 import { useSearchParams } from "react-router-dom";
 import Comments from "./Comments";
+import LiveChat from "./LiveChat";
 
 const WatchPage = () => {
   const dispatch = useDispatch();
@@ -12,18 +13,23 @@ const WatchPage = () => {
   }, []);
 
   return (
-    <div className="flex flex-col">
+    <div className="flex flex-col w-full">
       <div className="px-5 flex w-full">
-        <iframe
-          width="1200"
-          height="500"
-          src={"https://www.youtube.com/embed/" + searchParams.get("v")}
-          title="YouTube video player"
-          frameBorder="0"
-          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-          referrerPolicy="strict-origin-when-cross-origin"
-          allowFullScreen
-        ></iframe>
+        <div className="">
+          <iframe
+            width="1200"
+            height="525"
+            src={"https://www.youtube.com/embed/" + searchParams.get("v")}
+            title="YouTube video player"
+            frameBorder="0"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+            referrerPolicy="strict-origin-when-cross-origin"
+            allowFullScreen
+          ></iframe>
+        </div>
+        <div className="w-full">
+          <LiveChat />
+        </div>
       </div>
       <Comments />
     </div>
