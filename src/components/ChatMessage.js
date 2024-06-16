8a0b7aaefdac64b90333749e71f4c1080da3c0c8
saijamii <saijami38@gmail.com>
2024-06-16 15:15:19 +0530

    changes

--- src/components/ChatMessage.js
+++ src/components/ChatMessage.js
@@ -2,7 +2,7 @@ const ChatMessage = ({ name, message }) => {
   return (
     <div className="flex items-center shadow-sm p-2">
       <img
-        className="h-7"
+        className="h-5"
         alt="user"
         src="https://www.iconpacks.net/icons/2/free-user-icon-3296-thumb.png"
       />
