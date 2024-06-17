81581e3a372809b34665badf73d1c037246b7f5d
saijamii <saijami38@gmail.com>
2024-06-17 18:09:38 +0530

    changes

--- src/Utils/constants.js
+++ src/Utils/constants.js
@@ -1,6 +1,6 @@
 const GOOGLE_API_KEY = "AIzaSyDo6l411i4StVx3BugFdqjOWVKIEJdEG0c";
 export const LIVE_CHAT_LIMIT = 20;
 
-export const YOUTUBE_VIDEO_API = `https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=50&regionCode=IN&key=${GOOGLE_API_KEY}`;
+export const YOUTUBE_VIDEO_API = `https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=10&regionCode=IN&key=${GOOGLE_API_KEY}`;
 export const AUTOCOMPLETE_API =
   "http://suggestqueries.google.com/complete/search?client=firefox&ds=yt&q=";
