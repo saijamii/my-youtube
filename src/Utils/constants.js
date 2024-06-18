27fb1f8612bfe67f3602239273e4850f745ce471
saijami <saijami@unosimple.com>
2024-06-18 19:06:13 +0530

    key change

--- src/Utils/constants.js
+++ src/Utils/constants.js
@@ -1,4 +1,4 @@
-const GOOGLE_API_KEY = "AIzaSyDo6l411i4StVx3BugFdqjOWVKIEJdEG0c";
+const GOOGLE_API_KEY = "AIzaSyCcdTRerjIE6OPD-SDI490elTlXeJNGBMo";
 export const LIVE_CHAT_LIMIT = 20;
 
 export const YOUTUBE_VIDEO_API = `https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=20&regionCode=IN&key=${GOOGLE_API_KEY}`;
