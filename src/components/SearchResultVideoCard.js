ec209906a717590fcf0822446f29e3bfd039e192
saijami <saijami@unosimple.com>
2024-06-18 19:06:22 +0530

    Link added

--- src/components/SearchResultVideoCard.js
+++ src/components/SearchResultVideoCard.js
@@ -1,38 +1,41 @@
 import React from "react";
 import { getDifference } from "../Utils/helper";
+import { Link } from "react-router-dom";
 
 const SearchResultVideoCard = ({ video }) => {
   if (!video || !video.snippet) {
     return null;
   }
 
-  const { snippet } = video;
+  const { snippet, id } = video;
   const { title, channelTitle, description, thumbnails, publishTime } = snippet;
 
   return (
-    <div className="flex flex-row min-h-48 ">
-      <img
-        className="m-2 p-2 w-1/7 rounded-2xl h-full max-h-64 w-full max-w-96"
-        src={thumbnails?.high?.url}
-        alt="video-thumbnail"
-      />
+    <Link to={`/watch/${id?.videoId}`}>
+      <div className="flex flex-row min-h-48">
+        <img
+          className="m-2 p-2 w-1/7 rounded-2xl h-full max-h-64 w-full max-w-96"
+          src={thumbnails?.high?.url}
+          alt="video-thumbnail"
+        />
 
-      <div className="px-2 py-4">
-        <h2 className="font-bold">{title}</h2>
-        <div className="flex items-center">
-          <img
-            className="rounded-full w-7 h-7 mt-2 mr-2"
-            alt="channel"
-            src={thumbnails?.default?.url}
-          />
-          <p>{channelTitle}</p>
-          <p className="mx-2 font-semibold">
-            {getDifference(publishTime) + " ago"}
-          </p>
+        <div className="px-2 py-4">
+          <h2 className="font-bold">{title}</h2>
+          <div className="flex items-center">
+            <img
+              className="rounded-full w-7 h-7 mt-2 mr-2"
+              alt="channel"
+              src={thumbnails?.default?.url}
+            />
+            <p>{channelTitle}</p>
+            <p className="mx-2 font-semibold">
+              {getDifference(publishTime) + " ago"}
+            </p>
+          </div>
+          <p className="text-sm">{description}</p>
         </div>
-        <p className="text-sm">{description}</p>
       </div>
-    </div>
+    </Link>
   );
 };
 
